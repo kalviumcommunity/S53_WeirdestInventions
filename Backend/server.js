9c9c5dd193987e6cac7023b4882b7916394f34b5
jason william <jasonwilliam@jasons-MacBook-Air.local>
2024-02-06 18:14:44 +0530

    atlas done and mongodb connected

--- Backend/server.js
+++ Backend/server.js
@@ -1,11 +1,27 @@
 const express = require('express')
 const app = express()
 
+const mongoose = require("mongoose");
+require("dotenv").config()
+
+async function main(){
+    await mongoose.connect(process.env.MONGO_KEY)
+}
+
+console.log(process.env.MONGO_KEY);
+
+
 app.get('/ping',(req,res)=>{
     res.send("pong")
 })
 app.get('/',(req,res)=>{
-    res.send("Home")
+    main()
+.then(()=>{
+    res.send("Database Connected Successfully!");
+})
+.catch((err)=>{
+    res.send(err);
+})
 })
 
 app.listen(3000,()=>{
