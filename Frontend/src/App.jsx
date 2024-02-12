4160c8ae7a5cac05d9f5ba583f15d36c128bf398
jason william <jasonwilliam@jasons-MacBook-Air.local>
2024-02-12 15:38:16 +0530

    done deploying

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,16 +1,20 @@
+import { Router } from 'react-router-dom'
 
 import './App.css'
 import Home from './components/Home'
 import Navbar from './components/Navbar'
 import './index.css'
+import Card from './components/cards'
 
 function App() {
 
 
   return (
-    <>
+    <><div style={{width: "100vw",height: "100vh",padding: "0"}} className='tab'>
       <Navbar/>
-      <Home/>
+      {/* <Home/> */}
+      <Card/>
+    </div>
     </>
   )
 }
