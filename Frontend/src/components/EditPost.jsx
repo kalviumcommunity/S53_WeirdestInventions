a61e93e015e9d7aaeee80e7c5a7f8841801388fe
jason william <jasonwilliam@jasons-MacBook-Air.local>
2024-02-19 10:50:38 +0530

    for error

--- Frontend/src/components/EditPost.jsx
+++ Frontend/src/components/EditPost.jsx
@@ -91,6 +91,8 @@ const EditPost = () => {
 
 
 
+
+
                     <button
                        
                         className="mt-6 block w-full select-none rounded-lg bg-red-400 py-3 px-6 text-center align-middle font-sans text-xs font-bold uppercase text-white shadow-md shadow-blue-500/20 transition-all hover:shadow-lg hover:shadow-blue-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
